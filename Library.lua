725f7cdc83767202ac085d3726835d8d555876ea
noregret15 <artemfaizov54@gmail.com>
2026-01-22 14:04:19 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -37,7 +37,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 80, 80);
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.SourceSansBold;
+    Font = Enum.Font.Ubuntu;
 
     OpenedFrames = {};
     DependencyBoxes = {};
