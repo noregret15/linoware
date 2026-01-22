a42b599538fe6226bf4c3d05d112ba6a15655e5a
noregret15 <artemfaizov54@gmail.com>
2026-01-22 14:01:56 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -31,7 +31,7 @@ local Library = {
 
     FontColor = Color3.fromRGB(245, 245, 245);
     MainColor = Color3.fromRGB(20, 20, 20);
-    BackgroundColor = Color3.fromRGB(40, 40, 40);
+    BackgroundColor = Color3.fromRGB(80, 80, 80);
     AccentColor = Color3.fromRGB(100, 150, 255);
     OutlineColor = Color3.fromRGB(60, 60, 60);
     RiskColor = Color3.fromRGB(255, 80, 80);
