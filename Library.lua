067e1b46c1fd732a409ca7f6274540790e36ccec
noregret15 <artemfaizov54@gmail.com>
2026-01-22 19:23:08 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,12 +32,12 @@ local Library = {
     FontColor = Color3.fromRGB(245, 245, 245);
     MainColor = Color3.fromRGB(20, 20, 20);
     BackgroundColor = Color3.fromRGB(15, 15, 15);
-    AccentColor = Color3.fromRGB(255, 0, 0);
+    AccentColor = Color3.fromRGB(100, 40, 90);
     OutlineColor = Color3.fromRGB(60, 60, 60);
     RiskColor = Color3.fromRGB(255, 80, 80);
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Ubuntu;
+    Font = Enum.Font.SourceSansBold;
 
     OpenedFrames = {};
     DependencyBoxes = {};
