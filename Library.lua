ad928cd54b868e5ebd37bbaa433860c08d752a27
noregret15 <artemfaizov54@gmail.com>
2026-01-23 15:07:20 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -37,7 +37,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 90, 90);
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.SourceSansSemibold;
+    Font = Enum.Font.ArimoBold;
 
     OpenedFrames = {};
     DependencyBoxes = {};
